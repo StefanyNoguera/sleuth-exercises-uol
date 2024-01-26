7bed507e960c76a06dd4111494355820893e726a
Stefany Noguera <stefanynoguerar@gmail.com>
2024-01-25 20:57:46 -0500

    pairs done

--- sleuth pro/ghostly/ghostly 4/sketch.js
+++ sleuth pro/ghostly/ghostly 4/sketch.js
@@ -40,17 +40,12 @@ function setup() {
 function draw() {
     // add your for loop below
     for (var i = 0; i < 8; i++) {
-        for (var j = 0; j < 8; j++) {
-
-            if (i % 2 == 0) {
-                if (j % 2 == 0) {
-                    image(chessPieceImage, 690 + i * 100, 361 + j * 100);
-                }
-            } else {
-                if (j % 2 != 0) {
-                    image(chessPieceImage, 100 + i * 100, 100 + j * 100);
-                }
-            }
-        }
-    }
+        for (var j = 0; j < 5; j++) {
+          if (j % 2 == 0) {
+            image(chessPieceImage, 690 + i * 73, 361 + j * 60);
+          } else {
+            // image(chessPieceImage, 620 + i * 10, 100 + j * 100);
+          }
+      }
+  }
 }
