bb8983af83c2c8a7baa24b6514096593f9a3c522
Stefany Noguera <stefanynoguerar@gmail.com>
2024-01-25 20:36:24 -0500

    new challenge

---
+++ sleuth pro/ghostly/ghostly 3/sketch.js
@@ -0,0 +1,41 @@
+/*
+
+Officer: 4098078
+CaseNum: 501-2-92770561-4098078
+
+Case 501 - Marrieta Von Neuman - stage 3
+
+For Marrieta a different approach is needed again.
+Follow Madame McCarthy’s advice below.
+
+Marrieta was a doting mother but she was also mean player of dice.
+To speak to Marrieta beyond the grave you must place a dice in each of the dashed circles.
+Use the image() and the diceImage variable command to place each dice in its position.
+You will need to use two for loops in a nested pattern to create the grid.
+
+There are many possible ways of completing this task,
+but you should ONLY use the following commands:
+
+ * for()
+ * image()
+
+*/
+
+var backgroundImg, diceImage;
+
+function preload() {
+    backgroundImg = loadImage("background.jpg");
+    diceImage = loadImage("Dice.png");
+}
+
+function setup() {
+    createCanvas(backgroundImg.width, backgroundImg.height);
+    image(backgroundImg, 0, 0);
+
+    imageMode(CENTER);
+}
+
+function draw() {
+    // add your for loop below
+
+}
