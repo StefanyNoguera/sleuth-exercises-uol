241407030b042e0e4580f41b4fb53e030bb42d14
Stefany Noguera <stefanynoguerar@gmail.com>
2024-02-14 11:16:32 -0500

    driveCar function

--- sleuth pro/vanishing vannevar/vanishing vannevar 3/sketch.js
+++ sleuth pro/vanishing vannevar/vanishing vannevar 3/sketch.js
@@ -29,19 +29,23 @@ vehicleObjects_list to understand their properties.
 function driveCar()
 {
 	/*
-	This function should do the following: 
-	 - increment detective_carObject's dist_driven property by its gas_val property 
+	This function should do the following:
+	 - increment detective_carObject's dist_driven property by its gas_val property
 	 - add a random amount between -0.01 and 0.01 to detective_carObject's vibrate_val property
 	 - use the constrain function to constrain detective_carObject's vibrate_val property to values between 0.09 and 0.97
 	 - call the turnoverCarEngine function passing detective_carObject as an argument
 	*/
+  detective_carObject.dist_driven += detective_carObject.gas_val;
+  detective_carObject.vibrate_val += random(-0.01, 0.01);
+  detective_carObject.vibrate_val = constrain(detective_carObject.vibrate_val, 0.09, 0.97);
+  turnoverCarEngine(detective_carObject);
 }
 
 
 function moveLanes(target_vehicle)
 {
 	/*
-	This function should do the following: 
+	This function should do the following:
 	 - move target_vehicle from one lane to the other.
 	 - do the move in a single step without any extra animation.
 	 - use lane_pos_a and lane_pos_b to effect the change.
@@ -53,7 +57,7 @@ function moveLanes(target_vehicle)
 function vehicleInfront( vehicle_a, vehicle_b )
 {
 	/*
-	This function should do the following: 
+	This function should do the following:
 	 - determine if vehicle_a is in the same lane and less than 200px behind vehicle_b.
 	 - do this by comparing the two cars' dist_driven properties
 	 - if these requirements are met then return the number_plate property for vehicle_b. Otherwise return false.
@@ -64,7 +68,7 @@ function vehicleInfront( vehicle_a, vehicle_b )
 function vehicleAtSide( target_car )
 {
 	/*
-	This function should do the following: 
+	This function should do the following:
 	 - traverse vehicleObjects_list and determine if any of the cars are parallel with target_car.
 	 - if a car is found to be parallel to target_car then return that car object.
 	 - cars are considered parallel if the absolute difference between their dist_driven properties is less than 25 px and they have non-matching x properties	*/
@@ -74,7 +78,7 @@ function vehicleAtSide( target_car )
 function identifySuspect()
 {
 	/*
-	This function should do the following: 
+	This function should do the following:
 	 - Check cars passing parallel to detective_carObject to see if they match the number_plate property in the suspect description.
 	 - it does this by calling vehicleAtSide.
 	 - if a positive result is returned then the number_plate property of the found car is then checked against the suspect description.
@@ -96,7 +100,7 @@ var carImages = {};
 var suspect;
 
 var vehicleObjects_list = [
-{ x: 500, y: 0, dist_driven: -200, car_classification: 'whiteCar', number_plate: 'WRLAIA', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 200, car_classification: 'greenCar', number_plate: 'RGPM9D', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 600, car_classification: 'blueCar', number_plate: 'LHCOTV', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 1000, car_classification: 'redCar', number_plate: 'FRLNG9', gas_val: 2, exhaust: [  ]} , { x: 300, y: 0, dist_driven: 1400, car_classification: 'blueCar', number_plate: '9CGGZW', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 1800, car_classification: 'greenCar', number_plate: 'B27IXW', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 2200, car_classification: 'whiteCar', number_plate: 'MDGQ5U', gas_val: 2, exhaust: [  ]} , { x: 300, y: 0, dist_driven: 2600, car_classification: 'greenCar', number_plate: 'YR8OUO', gas_val: 2, exhaust: [  ]} , { x: 300, y: 0, dist_driven: 3000, car_classification: 'greenCar', number_plate: 'PCNUO3', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 3400, car_classification: 'redCar', number_plate: 'M3JJTL', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 3800, car_classification: 'blueCar', number_plate: 'LUZD4O', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 4200, car_classification: 'blueCar', number_plate: '5S10YK', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 4600, car_classification: 'whiteCar', number_plate: 'NAB36I', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 5000, car_classification: 'whiteCar', number_plate: 'YT8Y1E', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 5400, car_classification: 'whiteCar', number_plate: 'B8KE2X', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 5800, car_classification: 'whiteCar', number_plate: 'Y322JB', gas_val: 2, exhaust: [  ]} , { x: 300, y: 0, dist_driven: 6200, car_classification: 'whiteCar', number_plate: '6GXSM6', gas_val: 2, exhaust: [  ]} , { x: 300, y: 0, dist_driven: 6600, car_classification: 'whiteCar', number_plate: 'CJFGWM', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 7000, car_classification: 'greenCar', number_plate: '1N2U6U', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 7400, car_classification: 'redCar', number_plate: 'HZZ919', gas_val: 2, exhaust: [  ]} 
+{ x: 500, y: 0, dist_driven: -200, car_classification: 'whiteCar', number_plate: 'WRLAIA', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 200, car_classification: 'greenCar', number_plate: 'RGPM9D', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 600, car_classification: 'blueCar', number_plate: 'LHCOTV', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 1000, car_classification: 'redCar', number_plate: 'FRLNG9', gas_val: 2, exhaust: [  ]} , { x: 300, y: 0, dist_driven: 1400, car_classification: 'blueCar', number_plate: '9CGGZW', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 1800, car_classification: 'greenCar', number_plate: 'B27IXW', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 2200, car_classification: 'whiteCar', number_plate: 'MDGQ5U', gas_val: 2, exhaust: [  ]} , { x: 300, y: 0, dist_driven: 2600, car_classification: 'greenCar', number_plate: 'YR8OUO', gas_val: 2, exhaust: [  ]} , { x: 300, y: 0, dist_driven: 3000, car_classification: 'greenCar', number_plate: 'PCNUO3', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 3400, car_classification: 'redCar', number_plate: 'M3JJTL', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 3800, car_classification: 'blueCar', number_plate: 'LUZD4O', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 4200, car_classification: 'blueCar', number_plate: '5S10YK', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 4600, car_classification: 'whiteCar', number_plate: 'NAB36I', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 5000, car_classification: 'whiteCar', number_plate: 'YT8Y1E', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 5400, car_classification: 'whiteCar', number_plate: 'B8KE2X', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 5800, car_classification: 'whiteCar', number_plate: 'Y322JB', gas_val: 2, exhaust: [  ]} , { x: 300, y: 0, dist_driven: 6200, car_classification: 'whiteCar', number_plate: '6GXSM6', gas_val: 2, exhaust: [  ]} , { x: 300, y: 0, dist_driven: 6600, car_classification: 'whiteCar', number_plate: 'CJFGWM', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 7000, car_classification: 'greenCar', number_plate: '1N2U6U', gas_val: 2, exhaust: [  ]} , { x: 500, y: 0, dist_driven: 7400, car_classification: 'redCar', number_plate: 'HZZ919', gas_val: 2, exhaust: [  ]}
 ];
 
 
@@ -130,7 +134,7 @@ function setup()
 	lane_pos_a = 300;
 	lane_pos_b = 500;
 
-	detective_carObject = 
+	detective_carObject =
 	{
 		x: roadLeftEdge + roadWidth/4,
 		y: 550,
