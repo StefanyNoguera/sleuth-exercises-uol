e4c9827578cedc4a93f3220e5d93d3a3a40b420e
Stefany Noguera <stefanynoguerar@gmail.com>
2024-02-22 12:25:49 -0500

    eighth found

--- sleuth pro/cooley gang/cooley gang 1/sketch.js
+++ sleuth pro/cooley gang/cooley gang 1/sketch.js
@@ -54,6 +54,7 @@ function draw()
   operaBuffs[7][4].selected = true;
   operaBuffs[8][5].selected = true;
   operaBuffs[8][6].selected = true;
+  operaBuffs[8][8].selected = true;
 
 
 	///////////DON'T CHANGE ANY CODE BELOW HERE////////////
